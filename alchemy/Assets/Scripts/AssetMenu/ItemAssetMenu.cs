aaf38589775898fed84ecb02040d7ba3dfced02a
DESKTOP-TJL9B8K\MyStery <rrr004rrr@gmail.com>
2018-05-06 09:58:22 +0800

    基本Item數值

--- alchemy/Assets/Scripts/AssetMenu/ItemAssetMenu.cs
+++ alchemy/Assets/Scripts/AssetMenu/ItemAssetMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,28 +7,42 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ItemAssetMenu : ScriptableObject
 {
-
-
-    [System.Serializable]
+    [Serializable]
     public class Item
     {
-        public enum EPYT
-        {
-            A,
-            B,
-            C,
-            D
-        }
+        //名字
+        public String Name;         
 
-        public int Number;
-        public string Name;
-        public EPYT XXX = new EPYT();
-        public int Infomation;    
-        public string Price;
+        /// <summary>
+        /// 屬性
+        /// 0.無
+        /// 1.
+        /// 2.
+        /// 3.
+        /// 4.
+        /// 5.
+        /// </summary>
+        public int Attributes;
+
+        /// <summary>
+        /// 物品等級  (介於1~10)
+        /// </summary>
+        public int ItemLevel;
+
+        //圖片
+        public Sprite sprite;
+
+        //敘述
+        public String Description;
     }
 
-
-
-    public List<Item> items;
+    public Item items = new Item
+    {
+        Name = "空氣",
+        Attributes = 0,
+        ItemLevel = 1,
+        sprite = null,
+        Description = "測試測試，這是空氣。"
+    };
 }
 
---
+++ alchemy/Assets/Scripts/RefineCanvas.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RefineCanvas : MonoBehaviour
+{
+    //背包物件
+    public List<ItemAssetMenu> BackpackItem ;
+
+
+
+    /// <summary>
+    /// 初始化背包物件
+    /// </summary>
+    void InitBackpackItems ()
+    {
+        //將背包還原初始狀態
+        InitBackpack();
+
+        //獲取背包數據
+        //BackpackItem =
+
+        //如果背包有東西，則生成；沒有則中止。
+        if (BackpackItem.Count > 0)
+            CreateBackpackItems();
+        else
+            return;
+
+        //檢查背包是否生成成功
+        if (CheckBackpackItemsChange())
+            InitBackpackItems();
+    }
+
+    /// <summary>
+    /// 初始化背包
+    /// </summary>
+    void InitBackpack ()
+    {
+
+    }
+
+    /// <summary>
+    /// 生成背包物件
+    /// </summary>
+    void CreateBackpackItems ()
+    {
+
+    }
+
+    /// <summary>
+    /// 刷新背包物件
+    /// </summary>
+    void ReflashBackpackItems ()
+    {
+
+    }
+
+    /// <summary>
+    /// 檢查背包是否有異
+    /// </summary>
+    bool CheckBackpackItemsChange ()
+    {
+
+        return true;
+    }
+}
