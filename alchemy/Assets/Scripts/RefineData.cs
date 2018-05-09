2a67ad22b1324c7152074c4e11f3b8ca64029afb
DESKTOP-TJL9B8K\MyStery <rrr004rrr@gmail.com>
2018-05-09 22:56:08 +0800

    基本讀取文檔

---
+++ alchemy/Assets/Scripts/RefineData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RefineData
+{
+    public class ReadRefineData
+    {
+        public static TextAsset textAsset;
+        public static string[] objInfoArray;
+
+        public static void LoadRes()
+        {
+            textAsset = (TextAsset)Resources.Load("RefineData");
+            objInfoArray = textAsset.text.Split('\n');
+        }
+
+        /// <summary>
+        /// 取得某行數值
+        /// </summary>
+        /// <param name="line">行數</param>
+        /// <returns></returns>
+        public static string ReadLine (int line)
+        {
+            LoadRes();
+            return objInfoArray[line];
+        }
+
+        /// <summary>
+        /// 取得數量
+        /// </summary>
+        /// <returns></returns>
+        public static int GetCount()
+        {
+            LoadRes();
+            return objInfoArray.Length;
+        }
+    }
+}
+
--- alchemy/Assets/Scripts/RefinePlace.cs
+++ alchemy/Assets/Scripts/RefinePlace.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RefineData;
 
 public class RefinePlace : MonoBehaviour {
 
     public ItemAssetMenu items;
 
+    private void Start()
+    {
+        print(ReadRefineData.ReadLine(0));
+    }
 
 }
