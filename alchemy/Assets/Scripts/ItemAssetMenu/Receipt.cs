f94a7fc900de46ed79435d03be23d90c177e27c1
jas78987 <jas78987@gmail.com>
2018-05-26 10:56:38 +0800

    配方class

---
+++ alchemy/Assets/Scripts/ItemAssetMenu/Receipt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Enum;
+
+namespace Variable
+{
+    /// <summary>
+    /// 配方
+    /// </summary>
+    public class Receipt
+    {
+        /// <summary>
+        /// 合成結果
+        /// </summary>
+        public Items ResultItem { get; set; }
+
+        /// <summary>
+        /// 副素材屬性
+        /// </summary>
+        public List<Arrtibutes> OtherAttributes{ get; set; }
+
+        /// <summary>
+        /// 合成用權重
+        /// </summary>
+        public int RefineWeight{ get; set; }
+
+    }
+
+}
