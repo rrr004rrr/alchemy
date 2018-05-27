fba10a9243fdf100f25fd07ceac01b301b26a5ac
DESKTOP-TJL9B8K\MyStery <rrr004rrr@gmail.com>
2018-05-27 10:01:17 +0800

    商店資料載入

--- alchemy/Assets/Scripts/ShopItemAdapter.cs
+++ alchemy/Assets/Scripts/ShopItemAdapter.cs
@@ -11,23 +11,28 @@ public class Shop
 
 public class ShopItemAdapter : MonoBehaviour {
 
+    [Header("商品預置物")]
+    public GameObject ShopItemPrefabs;
+
+    [Header("各商店的AssetMenu")]
     public List<ShopAssetMenu> ShopAssetMenus;
-
-    public  GameObject ShopItemPrefabs;
-
-    public Transform Content;
-
-    public GameObject ScrollView;
-
-    public int Count = 20;
 
     /// <summary>
     /// 商店販賣物
     /// </summary>
     private List<GameObject> shopItems;
 
-	// Use this for initialization
-	public void Start () {
+    public Transform Content;
+
+    private string itemName;
+    private Enum.Arrtibutes arrtibute;
+    private int itemLevel;
+    private Sprite icon;
+    private string description;
+    private int price;
+
+    // Use this for initialization
+    public void Start () {
         initShopItems("City_A");
     }
 	
@@ -36,7 +41,7 @@ public class ShopItemAdapter : MonoBehaviour {
 		
 	}
 
-    private void initShopItems(string cityName)
+    public void initShopItems(string cityName)
     {
         shopItems = new List<GameObject>();
 
@@ -46,8 +51,25 @@ public class ShopItemAdapter : MonoBehaviour {
             {
                 foreach(ItemAssetMenu i in m.item)
                 {
+                    #region 取值
+                    itemName = i.item.Name;
+                    arrtibute = i.item.Arrtibute;
+                    itemLevel = i.item.ItemLevel;
+                    icon = Resources.Load<Sprite>("Sprites/itemIcon/itemIcon_" + i.item.Icon);
+                    description = i.item.Description;
+                    price = i.Price;
+                    #endregion
+
                     GameObject shopItem = Instantiate(ShopItemPrefabs, Content);
-                    shopItem.transform.GetChild(1).GetChild(1).GetChild(0).GetComponent<Text>().text = i.item.Name;
+
+                    shopItem.transform.GetChild(1).GetChild(0).GetChild(1).GetComponent<Image>().sprite = icon;
+
+                    shopItem.transform.GetChild(1).GetChild(1).GetChild(0).GetComponent<Text>().text = "物品名稱："+itemName;
+                    shopItem.transform.GetChild(1).GetChild(1).GetChild(1).GetComponent<Text>().text = "物品等級：" + itemLevel.ToString();
+                    shopItem.transform.GetChild(1).GetChild(1).GetChild(2).GetComponent<Text>().text = description;
+
+                    shopItem.transform.GetChild(2).GetChild(0).GetComponent<Text>().text = "總價：" + price.ToString();
+
                     shopItems.Add(shopItem);
                 }            
             }
